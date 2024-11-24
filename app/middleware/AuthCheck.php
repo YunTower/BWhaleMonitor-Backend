860f03ac019024084f76fa99dac7f65fc3d3a649
fishyyds <286267038@qq.com>
2024-11-24 23:11:04 +0800

    add(middleware): 添加AuthCheck中间件全局安装及授权状态

---
+++ app/middleware/AuthCheck.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\middleware;
+
+use Webman\MiddlewareInterface;
+use Webman\Http\Response;
+use Webman\Http\Request;
+
+class AuthCheck implements MiddlewareInterface
+{
+    public function process(Request $request, callable $next): Response
+    {
+        if (!check_install()) {
+            return forbidden('系统未安装');
+        }
+        return $next($request);
+    }
+}

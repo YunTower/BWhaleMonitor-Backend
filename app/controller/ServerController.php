28c19a4a0e4d35beb65d90d4ead3da3f13a73670
fishyyds <286267038@qq.com>
2024-12-05 09:48:30 +0800

    feat(server): 添加服务器信息校验和唯一性检查
    -增加了对服务器名称、IP、操作系统和位置的非空验证
    - 对服务器IP进行唯一性检查，防止重复添加
    -引入异常处理机制，确保服务器错误能够被正确记录和响应- 使用Respect\Validation进行数据验证，提高代码可读性和维护性

--- app/controller/ServerController.php
+++ app/controller/ServerController.php
@@ -2,7 +2,12 @@
 
 namespace app\controller;
 
+use app\model\Server;
+use Respect\Validation\Exceptions\ValidationException;
+use Respect\Validation\Validator as v;
+use support\Log;
 use support\Request;
+use Exception;
 
 class ServerController
 {
@@ -21,7 +26,29 @@ class ServerController
 
     public function add(Request $request): \support\Response
     {
-        return json(['code' => 0, 'msg' => 'ok']);
+        try {
+            try {
+                $v = v::input($request->post(), [
+                    'name' => v::notEmpty()->setName('name'),
+                    'ip' => v::notEmpty()->ip()->setName('ip'),
+                    'os' => v::notEmpty()->setName('os'),
+                    'location' => v::notEmpty()->setName('location')
+                ]);
+            } catch (ValidationException $e) {
+                return badRequest($e->getMessage());
+            }
+
+            $check_ip = Server::where('ip', $v['ip'])->first();
+            if ($check_ip) {
+                return badRequest('已存在相同IP地址的服务器');
+            }
+
+            Server::create($v);
+            return success();
+        } catch (Exception $e) {
+            Log::error($e->getMessage(), ['error' => $e->getMessage(), 'line' => $e->getLine(), 'code' => $e->getCode(), 'file' => $e->getFile()]);
+            return serverError($e->getMessage());
+        }
     }
 
     public function delete(Request $request): \support\Response

a9e0c61eb11cc39877a3ed46e240506dabcdc617
fish <xyfishcn@qq.com>
2024-12-06 16:40:06 +0800

    refactor(install): 重构安装过程并更新 .gitignore
    - 更新 .gitignore 文件，将 install.lock 改为 install.lock.json- 重新组织 InstallController 中的代码结构，提高可读性和维护性
    - 移除了数据库表创建、检查和初始数据插入的注释代码
    - 优化了代码格式，减少了不必要的空行和注释

--- app/controller/InstallController.php
+++ app/controller/InstallController.php
@@ -107,59 +107,59 @@ class InstallController
             /**
              * 创建数据表
              */
-//            try {
-//                Db::schema()->create('yt_monitor_server', function (Blueprint $table) {
-//                    $table->increments('id');
-//                    $table->string('name');
-//                    $table->string('os');
-//                    $table->string('ip');
-//                    $table->string('location')->nullable();
-//                    $table->string('cpu')->nullable();
-//                    $table->string('memory')->nullable();
-//                    $table->string('disk')->nullable();
-//                    $table->string('key');
-//                    $table->string('status');
-//                    $table->string('uptime');
-//                    $table->timestamps();
-//                });
-//                Db::schema()->create('yt_monitor_log', function (Blueprint $table) {
-//                    $table->increments('id');
-//                    $table->string('server_id');
-//                    $table->string('name');
-//                    $table->string('value');
-//                    $table->string('created');
-//                });
-//                Db::schema()->create('yt_monitor_config', function (Blueprint $table) {
-//                    $table->increments('id');
-//                    $table->string('name');
-//                    $table->string('value')->nullable();
-//                    $table->timestamps();
-//                });
-//            } catch (QueryException $e) {
-//                Log::error($e->getMessage(), ['error' => $e->getMessage(), 'file' => $e->getFile(), 'line' => $e->getLine(), 'code' => $e->getCode()]);
-//                return serverError('数据表创建失败：' . $e->getMessage());
-//            }
+            try {
+                Db::schema()->create('yt_monitor_server', function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->string('name');
+                    $table->string('os');
+                    $table->string('ip');
+                    $table->string('location')->nullable();
+                    $table->string('cpu')->nullable();
+                    $table->string('memory')->nullable();
+                    $table->string('disk')->nullable();
+                    $table->string('key');
+                    $table->string('status');
+                    $table->string('uptime');
+                    $table->timestamps();
+                });
+                Db::schema()->create('yt_monitor_log', function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->string('server_id');
+                    $table->string('name');
+                    $table->string('value');
+                    $table->string('created');
+                });
+                Db::schema()->create('yt_monitor_config', function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->string('name');
+                    $table->string('value')->nullable();
+                    $table->timestamps();
+                });
+            } catch (QueryException $e) {
+                Log::error($e->getMessage(), ['error' => $e->getMessage(), 'file' => $e->getFile(), 'line' => $e->getLine(), 'code' => $e->getCode()]);
+                return serverError('数据表创建失败：' . $e->getMessage());
+            }
 
             /**
              * 检查数据表是否存在
              */
-//            $check_tables = check_tables_existence('all', true);
-//            if (!$check_tables || is_array($check_tables)) {
-//                throw new Exception('数据表' . $check_tables[0] . '缺失，请检查数据库配置或重新安装');
-//            }
+            $check_tables = check_tables_existence('all', true);
+            if (!$check_tables || is_array($check_tables)) {
+                throw new Exception('数据表' . $check_tables[0] . '缺失，请检查数据库配置或重新安装');
+            }
 
             /**
              * 写入初始数据
              */
-//            $v['password'] = password_hash($v['password'], PASSWORD_DEFAULT);
-//            $data = $v + [
-//                    'interval' => 5,
-//                    'visitor' => false,
-//                    'visitor_password' => null
-//                ];
-//            foreach ($data as $key => $value) {
-//                Config::create(['name' => $key, 'value' => $value]);
-//            }
+            $v['password'] = password_hash($v['password'], PASSWORD_DEFAULT);
+            $data = $v + [
+                    'interval' => 5,
+                    'visitor' => false,
+                    'visitor_password' => null
+                ];
+            foreach ($data as $key => $value) {
+                Config::create(['name' => $key, 'value' => $value]);
+            }
 
             /**
              * 获取本机IP地址
@@ -191,7 +191,6 @@ class InstallController
             openssl_pkey_export($res, $private_key);
             $public_key = openssl_pkey_get_details($res);
             $public_key = $public_key["key"];
-
 
             /**
              * 生成锁定文件
